81db4c70efe69386bb01ea1e5abc779f3da9e8e7
asliyapdg-shop <asliyapdg@gmail.com>
2026-02-26 21:43:44 +0100

    Add homepage

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>ASLIYA Shop</h1>
+      <p>La plateforme e-commerce arrive bientôt 🚀</p>
+    </main>
+  );
+}
