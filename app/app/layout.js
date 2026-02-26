26d26dabe5ba0d845b4bb78d8cb907d9d980e123
asliyapdg-shop <asliyapdg@gmail.com>
2026-02-26 23:46:34 +0100

    Add root layout

---
+++ app/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "ASLIYA Shop",
+  description: "Boutique en ligne ASLIYA Shop",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+                   }
