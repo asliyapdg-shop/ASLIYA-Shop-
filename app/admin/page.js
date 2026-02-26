ad277f1656f18514d9edfb72672722b522b4661f
asliyapdg-shop <asliyapdg@gmail.com>
2026-02-26 23:14:42 +0100

    Create admin page

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,15 +1,3 @@
-export default function AdminDashboard() {
-  return (
-    <>
-      <h1>Dashboard</h1>
-      <p>Bienvenue dans l’espace admin ASLIYA Shop</p>
-
-      <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", gap: 20, marginTop: 20 }}>
-        <div style={{ background: "#fff", padding: 20 }}>Produits : 0</div>
-        <div style={{ background: "#fff", padding: 20 }}>Commandes : 0</div>
-        <div style={{ background: "#fff", padding: 20 }}>Clients : 0</div>
-        <div style={{ background: "#fff", padding: 20 }}>Chiffre d’affaires : 0 €</div>
-      </div>
-    </>
-  );
-    }
+export default function AdminPage() {
+  return <h1>ADMIN OK</h1>;
+}
